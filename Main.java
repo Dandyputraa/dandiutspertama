f7c7cccec5ce881d771343dcd93fd7d4177cfc93
Dandy Putra <dandypu07@gmail.com>
2023-05-18 02:02:35 +0700

    update class main

--- Main.java
+++ Main.java
@@ -4,17 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Mhs mhs = new Mhs("ini_username", "ini_password");
 
-        System.out.println("Nama Mhs: " + mhs.getNama());
-        System.out.println("Nim Mhs: " + mhs.getNim());
-        System.out.println("Smt Mhs: " + mhs.getSmt());
-        System.out.println("Username: " + mhs.getUsername());
-        System.out.println("Password: " + mhs.getPassword());
-
         mhs.setNama("Dandi Putra Utama");
         mhs.setNim("G.111.19.0039");
         mhs.setSmt(8);
-        mhs.setUsername("new_username");
-        mhs.setPassword("new_password");
 
         System.out.println("Nama Mhs: " + mhs.getNama());
         System.out.println("Nim Mhs: " + mhs.getNim());
